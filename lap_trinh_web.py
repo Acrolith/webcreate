2529407dbe3b171abce41b04419c1e1c403d6b9b
NhanLD <ledinhnhan2974@gmail.com>
2019-11-06 21:46:13 +0700

    upload image

--- lap_trinh_web.py
+++ lap_trinh_web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,send_file
 
 app = Flask(__name__)
 
@@ -12,5 +12,8 @@ def hello1():
 def hello():
     return render_template("index.html")
 
-
+@app.route('/123')
+def ledinhnhan():
+    path = "dota2.jpg"
+    return send_file (path)
 app.run(port=5001, host="0.0.0.0")
